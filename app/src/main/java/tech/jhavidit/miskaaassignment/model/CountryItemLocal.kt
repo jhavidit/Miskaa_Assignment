a1a93cea44597fb507ad119a2109a9d5c9cfcb89
Vidit Jha <jhavidit@gmail.com>
2020-11-21 17:09:31 +0530

    Added - Local Room Database

---
+++ app/src/main/java/tech/jhavidit/miskaaassignment/model/CountryItemLocal.kt
@@ -0,0 +1,4 @@
+package tech.jhavidit.miskaaassignment.model
+
+class CountryItemLocal {
+}
--- app/src/main/java/tech/jhavidit/miskaaassignment/room/CountryDatabase.kt
+++ app/src/main/java/tech/jhavidit/miskaaassignment/room/CountryDatabase.kt
@@ -1,5 +1,5 @@
 package tech.jhavidit.miskaaassignment.room
-//
+
 //import android.content.Context
 //import androidx.room.Database
 //import androidx.room.Room
